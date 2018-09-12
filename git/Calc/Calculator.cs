88937a76f4295fc82b5aafd3a81fa8b996f52964
Buzja <buzanov1998152@mail.ru>
2018-09-12 18:27:53 +0300

    git lab

---
+++ git/Calc/Calculator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Calc
+{
+    class Calculator
+    {
+        private double buffer;
+
+        public Calculator()
+        {
+            buffer = 0;
+        }
+
+        public bool checkBuf()
+        {
+            if (this.buffer != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public void putToBuf(double a)
+        {
+            this.buffer = a;
+        }
+
+        private void Multiply(double b)
+        {
+            this.buffer *= b;
+        }
+
+        private void Devision(double b)
+        {
+            this.buffer /= b;
+        }
+
+        private void Addition(double b)
+        {
+            this.buffer += b;
+        }
+
+        private void Subtration(double b)
+        {
+            this.buffer -= b;
+        }
+        
+        public Double ShowBuffer()
+        {
+            return this.buffer;
+        }
+
+        public void ShowResult()
+        {
+             Console.WriteLine("Result: "+this.buffer);
+        }
+
+        public void Reset()
+        {
+            this.buffer = 0;
+        }
+
+        public void ChooseOper(char oper, double b)
+        {
+            switch (oper)
+            {
+                case '+':
+                    {
+                        Addition(b);
+                        break;
+                    }
+                case '-':
+                    {
+                        Subtration(b);
+                        break;
+                    }
+                case '*':
+                    {
+                        Multiply(b);
+                        break;
+                    }
+                case '/':
+                    {
+                        Devision(b);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Unknown operation");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ git/Calc/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("Operations list:\n+ - addition\n- - subtration\n* - multiplication\n/ - devision\n= - show result\nr - reset buffer\nq-exit\n");
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Buffer = " + calc.ShowBuffer());
+                    bool check = calc.checkBuf();
+                    if (!check)
+                    {
+                        Console.WriteLine("Enter number:");
+                        double a = Convert.ToDouble(Console.ReadLine());
+                        calc.putToBuf(a);
+                    }
+                    Console.WriteLine("Enter operation:");
+                    Char oper = Convert.ToChar(Console.ReadLine());
+                    if (oper == '=')
+                    {
+                        calc.ShowResult();
+                    }
+                    else if (oper == 'r')
+                    {
+                        calc.Reset();
+                    }
+                    else if (oper == 'q')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter number:");
+                        double b = Convert.ToDouble(Console.ReadLine());
+                        calc.ChooseOper(oper, b);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Uncorrect number or operation");
+                }
+            }
+        }
+    }
+}
