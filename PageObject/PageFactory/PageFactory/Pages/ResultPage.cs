751d5246b91266aff27664471ea3818b1b774642
Buzja <buzanov1998152@mail.ru>
2018-11-18 17:50:53 +0300

    change names and add PageObject project

---
+++ PageObject/PageFactory/PageFactory/Pages/ResultPage.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageFactoryProject.Pages
+{
+    public class ResultPage
+    {
+        private IWebDriver driver;
+
+        public ResultPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = ".change-dates li.active > .day .price")]
+        private IWebElement activeTabPrice;
+
+        [FindsBy(How = How.CssSelector, Using = ".economy-price .price")]
+        private IList<IWebElement> prices;
+        
+        public bool checkMinPrice()
+        {
+            double minPrice = Convert.ToDouble(activeTabPrice.Text.Split('\n')[1].Split(' ')[0]);
+            double min = Convert.ToDouble(prices[0].Text.Split(' ')[0]);
+
+            foreach (var el in prices)
+            {
+                double price = Convert.ToDouble(el.Text.Split(' ')[0]);
+                if(price < min)
+                {
+                    min = price;
+                }
+            }
+
+            return (min == minPrice);
+        }
+    }
+}
---
+++ PageObject/PageFactory/PageFactory/Program.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using PageFactoryProject.Pages;
+
+namespace PageFactoryProject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+         
+        }
+    }
+
+    [TestFixture]
+    class Test
+    {
+        private const string url = @"https://belavia.by/";
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new FirefoxDriver();
+            driver.Url = url;
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void CheckCorrectActivePrice()
+        {
+            HomePage homePage = new HomePage(driver);
+            ResultPage resultPage = homePage.GoToResultPage("Минск", "Лондон", "10", "23");
+            Assert.IsTrue(resultPage.checkMinPrice());
+        }
+    }
+}
